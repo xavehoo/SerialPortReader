c3143107bc7630d11211d381bff66bdba59d31d3
Saverio Bavosio <xavehoo@yahoo.it>
2022-11-09 11:36:31 +0100

    Aggiunto test sulla scrittura

--- SerialPortReaderNetFramework/Form1.Designer.cs
+++ SerialPortReaderNetFramework/Form1.Designer.cs
@@ -29,82 +29,133 @@ namespace SerialPortReaderNetFramework
         /// </summary>
         private void InitializeComponent()
         {
-            this.startButton = new System.Windows.Forms.Button();
-            this.stopButton = new System.Windows.Forms.Button();
+            this.startReadButton = new System.Windows.Forms.Button();
+            this.stopReadButton = new System.Windows.Forms.Button();
             this.readTextBox = new System.Windows.Forms.TextBox();
             this.logTextBox = new System.Windows.Forms.TextBox();
             this.labelLog = new System.Windows.Forms.Label();
+            this.labelWrite = new System.Windows.Forms.Label();
+            this.writeTextBox = new System.Windows.Forms.TextBox();
+            this.labelRead = new System.Windows.Forms.Label();
+            this.writeButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // startButton
+            // startReadButton
             // 
-            this.startButton.Location = new System.Drawing.Point(12, 37);
-            this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(150, 50);
-            this.startButton.TabIndex = 0;
-            this.startButton.Text = "Start Reading";
-            this.startButton.UseVisualStyleBackColor = true;
-            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            this.startReadButton.Location = new System.Drawing.Point(12, 12);
+            this.startReadButton.Name = "startReadButton";
+            this.startReadButton.Size = new System.Drawing.Size(100, 50);
+            this.startReadButton.TabIndex = 0;
+            this.startReadButton.Text = "Start Reading";
+            this.startReadButton.UseVisualStyleBackColor = true;
+            this.startReadButton.Click += new System.EventHandler(this.startReadButton_Click);
             // 
-            // stopButton
+            // stopReadButton
             // 
-            this.stopButton.Enabled = false;
-            this.stopButton.Location = new System.Drawing.Point(12, 114);
-            this.stopButton.Name = "stopButton";
-            this.stopButton.Size = new System.Drawing.Size(150, 50);
-            this.stopButton.TabIndex = 1;
-            this.stopButton.Text = "Stop Reading";
-            this.stopButton.UseVisualStyleBackColor = true;
-            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            this.stopReadButton.Enabled = false;
+            this.stopReadButton.Location = new System.Drawing.Point(12, 68);
+            this.stopReadButton.Name = "stopReadButton";
+            this.stopReadButton.Size = new System.Drawing.Size(100, 50);
+            this.stopReadButton.TabIndex = 1;
+            this.stopReadButton.Text = "Stop Reading";
+            this.stopReadButton.UseVisualStyleBackColor = true;
+            this.stopReadButton.Click += new System.EventHandler(this.stopReadButton_Click);
             // 
             // readTextBox
             // 
-            this.readTextBox.Location = new System.Drawing.Point(12, 188);
+            this.readTextBox.Location = new System.Drawing.Point(137, 28);
             this.readTextBox.Multiline = true;
             this.readTextBox.Name = "readTextBox";
-            this.readTextBox.Size = new System.Drawing.Size(776, 250);
+            this.readTextBox.Size = new System.Drawing.Size(170, 90);
             this.readTextBox.TabIndex = 2;
             // 
             // logTextBox
             // 
-            this.logTextBox.Location = new System.Drawing.Point(206, 55);
+            this.logTextBox.Location = new System.Drawing.Point(12, 283);
             this.logTextBox.Multiline = true;
             this.logTextBox.Name = "logTextBox";
-            this.logTextBox.Size = new System.Drawing.Size(582, 109);
+            this.logTextBox.Size = new System.Drawing.Size(499, 95);
             this.logTextBox.TabIndex = 3;
             // 
             // labelLog
             // 
             this.labelLog.AutoSize = true;
-            this.labelLog.Location = new System.Drawing.Point(206, 37);
+            this.labelLog.Location = new System.Drawing.Point(12, 267);
             this.labelLog.Name = "labelLog";
-            this.labelLog.Size = new System.Drawing.Size(30, 15);
+            this.labelLog.Size = new System.Drawing.Size(28, 13);
             this.labelLog.TabIndex = 4;
             this.labelLog.Text = "Log:";
             // 
+            // labelWrite
+            // 
+            this.labelWrite.AutoSize = true;
+            this.labelWrite.Location = new System.Drawing.Point(501, 12);
+            this.labelWrite.Name = "labelWrite";
+            this.labelWrite.Size = new System.Drawing.Size(91, 13);
+            this.labelWrite.TabIndex = 5;
+            this.labelWrite.Text = "Write Some Data:";
+            // 
+            // writeTextBox
+            // 
+            this.writeTextBox.Location = new System.Drawing.Point(504, 28);
+            this.writeTextBox.Multiline = true;
+            this.writeTextBox.Name = "writeTextBox";
+            this.writeTextBox.Size = new System.Drawing.Size(170, 90);
+            this.writeTextBox.TabIndex = 6;
+            this.writeTextBox.TextChanged += new System.EventHandler(this.writeTextBox_TextChanged);
+            // 
+            // labelRead
+            // 
+            this.labelRead.AutoSize = true;
+            this.labelRead.Location = new System.Drawing.Point(134, 12);
+            this.labelRead.Name = "labelRead";
+            this.labelRead.Size = new System.Drawing.Size(62, 13);
+            this.labelRead.TabIndex = 7;
+            this.labelRead.Text = "Read Data:";
+            // 
+            // writeButton
+            // 
+            this.writeButton.Enabled = false;
+            this.writeButton.Location = new System.Drawing.Point(377, 46);
+            this.writeButton.Name = "writeButton";
+            this.writeButton.Size = new System.Drawing.Size(100, 50);
+            this.writeButton.TabIndex = 8;
+            this.writeButton.Text = "Write";
+            this.writeButton.UseVisualStyleBackColor = true;
+            this.writeButton.Click += new System.EventHandler(this.writeButton_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(686, 390);
+            this.Controls.Add(this.writeButton);
+            this.Controls.Add(this.labelRead);
+            this.Controls.Add(this.writeTextBox);
+            this.Controls.Add(this.labelWrite);
             this.Controls.Add(this.labelLog);
             this.Controls.Add(this.logTextBox);
             this.Controls.Add(this.readTextBox);
-            this.Controls.Add(this.stopButton);
-            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.stopReadButton);
+            this.Controls.Add(this.startReadButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
 
-        private System.Windows.Forms.Button startButton;
-        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button startReadButton;
+        private System.Windows.Forms.Button stopReadButton;
         private System.Windows.Forms.TextBox readTextBox;
         private System.Windows.Forms.TextBox logTextBox;
         private System.Windows.Forms.Label labelLog;
+        private System.Windows.Forms.Label labelWrite;
+        private System.Windows.Forms.TextBox writeTextBox;
+        private System.Windows.Forms.Label labelRead;
+        private System.Windows.Forms.Button writeButton;
     }
 }
 
--- SerialPortReaderNetFramework/Form1.cs
+++ SerialPortReaderNetFramework/Form1.cs
@@ -14,14 +14,19 @@ namespace SerialPortReaderNetFramework
     public partial class Form1 : Form
     {
         // Create the serial port with basic settings 
-        private SerialPort port = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);
+        private SerialPort port = new SerialPort("COM6", 9600, Parity.None, 8, StopBits.One);
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void startButton_Click(object sender, EventArgs e)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void startReadButton_Click(object sender, EventArgs e)
         {
             // Attach a method to be called when there
             // is data waiting in the port's buffer 
@@ -40,23 +45,72 @@ namespace SerialPortReaderNetFramework
             }
 
             // Enter an application loop to keep this thread alive 
-            startButton.Enabled = false;
-            stopButton.Enabled = true;
+            startReadButton.Enabled = false;
+            stopReadButton.Enabled = true;
         }
 
-        private void stopButton_Click(object sender, EventArgs e)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void stopReadButton_Click(object sender, EventArgs e)
         {
             port.DataReceived -= Port_DataReceived;
             port.Close();
             logTextBox.Text += $"{DateTimeOffset.Now} - Stop reading on port {port.PortName}\r\n";
-            startButton.Enabled = true;
-            stopButton.Enabled = false;
+            startReadButton.Enabled = true;
+            stopReadButton.Enabled = false;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void writeTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(((TextBox)sender).Text)) {
+                writeButton.Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void writeButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Begin communications 
+                port.Open();
+                logTextBox.Text += $"{DateTimeOffset.Now} - Start writing on port {port.PortName}\r\n";
+                port.Write(writeTextBox.Text);
+                logTextBox.Text += $"{DateTimeOffset.Now} - Written on port {port.PortName}\r\n";
+                port.Close();
+                writeTextBox.Text = "";
+                writeButton.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                logTextBox.Text += $"{DateTimeOffset.Now} - Error: {ex.Message}\r\n";
+                port.DataReceived -= Port_DataReceived;
+                return;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             // Show all the incoming data in the port's buffer
             this.readTextBox.Text += port.ReadExisting();
         }
+
     }
 }
